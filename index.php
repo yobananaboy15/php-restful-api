b8c37339d877d3a70f7d5f21fbf2da0315d2c26e
Axel Thiel <axth2220@gmail.com>
2021-05-02 12:42:35 +0200

    final product

--- index.php
+++ index.php
@@ -6,38 +6,36 @@ header("Access-Control-Allow-Methods: GET");
 header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
 header("Referrer-Policy: no-referrer");
 
-//Bättre att använda include?
-
 $data = file_get_contents('./data.json', FILE_USE_INCLUDE_PATH);
-$category_array = ['jewelery', 'men clothing', 'women clothing', 'electronics'];
-$array_data = json_decode($data, true);
-
-//Skapa en array med erros: Category doesn't exist, Show must be between 1-20.
+$returnArray = json_decode($data, true);
+$categoryArray = ['jewelery', 'men clothing', 'women clothing', 'electronics'];
 $error_array = [];
 
-$show = $_GET['show'] ?? -1;
-$category = $_GET['category'];
+$show = $_GET['show'] ?? null;
+$category = $_GET['category'] ?? null;
 
-//Kolla om array category isset.
 if ($category) {
-    if (!in_array($category, $category_array)) {
+    if (!in_array($category, $categoryArray)) {
         array_push($error_array, array("Category" => 'Category not found'));
     } else {
-        $returnArray = array_filter($array_data, function ($product) {
+        $returnArray = array_filter($returnArray, function ($product) {
             global $category;
             return $product['category'] == $category;
         });
     }
 }
 
-//Den borde inte göra det här om om det finns ett fel ovan.
-if (!$show >= 1 && $show <= 20) {
-    $array = json_decode($data);
-    shuffle($array);
-    $returnData = json_encode(array_slice($array, 0, $_GET['show']));
-    echo $returnData;
+if ($show) {
+    if ($show >= 1 && $show <= 20) {
+        shuffle($returnArray);
+        $returnArray = array_slice($returnArray, 0, $show);
+    } else {
+        array_push($error_array, array("Show" => 'Show must be between 1 and 20'));
+    }
 }
 
-// echo "<pre>";
-echo json_encode($error_array);
-// echo "</pre>";
+if (count($error_array)) {
+    echo json_encode($error_array);
+} else {
+    echo json_encode($returnArray);
+}
